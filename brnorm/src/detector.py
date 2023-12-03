5e2b9323b1263a9c2efb6d635b65ae8dac4b39d0
tadahayamiz <tadahaya@gmail.com>
2023-12-03 15:27:55 +0900

    detector

--- brnorm/src/detector.py
+++ brnorm/src/detector.py
@@ -6,6 +6,8 @@ Created on Fri 29 15:46:32 2022
 """
 import pandas as pd
 import numpy as np
+from scipy import stats
+from statsmodels.stats.multitest import multipletests
 
 class Data:
     """ data instance """
@@ -69,6 +71,14 @@ class Detector:
         self.tgt = Data.load(data)
 
 
+    def check_feature(self):
+        """ check whether features are the same or not"""
+        if self.grd.feature == self.tgt.feature:
+            return True
+        else:
+            return False
+
+
     def detect(self):
         """ detect differently expressed features """
         raise NotImplementedError
@@ -81,12 +91,29 @@ class TwoGroupDetector(Detector):
     """
     def __init__(self):
         super().__init__()
+        self.summary = None
     
 
-    def detect(self):
+    def detect(self, alpha:float=0.05, fold:float=1.0):
         """ detect differently expressed features """
-        raise NotImplementedError
-    
+        if not self.check_feature():
+            raise ValueError("!! features were not consistent between the ground truth and the target !!")
+        p = self.grd.n_feature
+        diff = np.zeros((p,))
+        pval = np.zeros((p,))
+        for i in range(p):
+            pval[i] = stats.ttest_ind(self.grd.X[i], self.tgt.X[i], equal_var=False)[1]
+            diff[i] = self.grd.X[i].mean() - self.tgt.X[i].mean()
+        qval = multipletests(pval, method="fdr_bh")[1]
+        res = pd.DataFrame(
+            {"diff":diff, "pval":pval, "qval":qval, "diff_abs":np.abs(diff), "name_feature":self.grd.feature},
+            )
+        res.loc["check_diff"] = res["diff_abs"] >= fold
+        res.loc["check_qval"] = res["qval"] <= alpha
+        res.loc["diff_feature"] = res.loc["check_diff"] & res.loc["check_qval"]
+        self.summary = res[["diff", "pval", "qval", "name_feature", "diff_feature"]].copy()
+        return list(res[res["diff_feature"]].index)
+
 
 class PCADetector(Detector):
     """
